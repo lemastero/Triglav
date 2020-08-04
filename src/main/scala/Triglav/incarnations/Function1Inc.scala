bb71a32df4aa22011bcecd3dc5120d3c0d1a92c1
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-08-04 16:29:55 +0200

    fix after rename of Strong profunctor

--- src/main/scala/Triglav/incarnations/Function1Inc.scala
+++ src/main/scala/Triglav/incarnations/Function1Inc.scala
@@ -1,11 +1,11 @@
 package Triglav.incarnations
 
 import Triglav.face2.Profunctor
-import Triglav.tambara.Strong
+import Triglav.tambara.CartesianStrong
 
 object Function1Inc {
 
-  def function1Profunctor: Profunctor[Function1] = new Strong[Function1] {
+  def function1Profunctor: Profunctor[Function1] = new CartesianStrong[Function1] {
 
     // profunctor
     override def dimap[S,T,A,B](ab: A => B)(sa: S => A, bt: B => T): S => T =

365013a2f3fbdf40905f82cc485d162c3ad36a54
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-08-04 16:26:25 +0200

    add Magnified laws

--- src/main/scala/Triglav/tambara/Magnified.scala
+++ src/main/scala/Triglav/tambara/Magnified.scala
@@ -1,12 +1,21 @@
 package Triglav.tambara
 
-trait Magnified[=>:[-_,+_]] extends Strong[=>:] with Choice[=>:] { // TambaraModule (C, Either[_,D])
+trait Magnified[=>:[-_,+_]] extends CartesianStrong[=>:] with CoCartesianChoice[=>:] { // TambaraModule (C, Either[_,D])
+
   def maginfyLS[A,B,C,D]: (A =>: B) => ((C, Either[A,D]) =>: (C, Either[B,D])) =
-    second compose left
+    second[Either[A,D], Either[B,D], C] compose left[A,B,D]
+
   def maginfyRS[A,B,C,D]: (A =>: B) => ((C, Either[D,A]) =>: (C, Either[D,B])) =
-    second compose right
+    second[Either[D,A], Either[D,B], C] compose right[A,B,D]
+
   def maginfyLF[A,B,C,D]: (A =>: B) => ((Either[A,D],C) =>: (Either[B,D],C)) =
-    first compose left
+    first[Either[A,D], Either[B,D], C] compose left[A,B,D]
+
   def maginfyRF[A,B,C,D]: (A =>: B) => ((Either[D,A],C) =>: (Either[D,B],C)) =
-    first compose right
+    first[Either[D,A], Either[D,B], C] compose right[A,B,D]
 }
+
+trait MagnifiedLaws[P[-_,+_]]
+  extends Magnified[P]
+    with CartesianStrong[P]
+    with CoCartesianChoice[P]

00b8f1fb024bf0c6dfd74c657b8b9e6cc074eef2
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-08-04 16:30:44 +0200

    fix variance in FunList and extended optics

--- src/main/scala/Triglav/eye/classic/Optics.scala
+++ src/main/scala/Triglav/eye/classic/Optics.scala
@@ -4,6 +4,6 @@ object Optics {
   case class Iso       [-S,+T,+A,-B](       from:   S => A,            to:     B     => T )
   case class Prism     [-S,+T,+A,-B](       matsh:  S => Either[T,A],  revGet: B     => T )
   case class Lens      [-S,+T,+A,-B](       view:   S => A,            update: (B,S) => T )
-  case class Traversal [S,T,A,B]    (    extract:   S => FunList[A,B,T]                   )
-  case class Optional  [S,A]        (        get:   S => Option[A],    set:    (A,S) => S )
+  case class Traversal [-S,+T,+A,-B](    extract:   S => FunList[A,B,T]                   )
+  case class Optional  [-S,+T,+A,-B](        get:   S => Option[A],    set:    (B,S) => T )
 }

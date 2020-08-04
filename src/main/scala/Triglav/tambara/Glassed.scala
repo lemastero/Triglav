92401c6b82434bd9ed315e64b96963de16f1ce0e
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-08-04 16:29:28 +0200

    add failed experiment with Closed profunctor laws

--- src/main/scala/Triglav/tambara/Glassed.scala
+++ src/main/scala/Triglav/tambara/Glassed.scala
@@ -1,8 +1,21 @@
 package Triglav.tambara
 
-trait Glassed[=>:[-_,+_]] extends Strong[=>:] with Closed[=>:] { // TambaraModule (T, U => _)
-  def glassedL[A,B,U,T]: (A =>: B) => ((T, U => A) =>: (T, U => B)) =
-    second compose closed
-  def glassedR[A,B,U,T]: (A =>: B) => ((U => A,T) =>: (U => B,T)) =
-    first compose closed
+import Triglav.face2.ProfunctorLaws
+
+trait Glassed[P[-_,+_]] extends CartesianStrong[P] with Closed[P] { // TambaraModule (T, U => _)
+
+  def glassedL[A,B,U,T]: P[A,B] => P[(T, U => A), (T, U => B)] = {
+    second[U=>A,U=>B,T] compose closed[A,B,U]
+  }
+
+  def glassedR[A,B,U,T]: P[A,B] => P[(U => A,T), (U => B,T)] = {
+    first[U=>A,U=>B,T] compose closed[A,B,U]
+  }
+}
+
+trait GlassedLaws[P[-_,+_]]
+  extends Glassed[P]
+    with ProfunctorLaws[P] {
+
+  // TODO
 }
--- src/main/scala/Triglav/tambara/Windowed.scala
+++ src/main/scala/Triglav/tambara/Windowed.scala
@@ -1,6 +1,6 @@
 package Triglav.tambara
 
-trait Windowed[=>:[-_,+_]] extends Choice[=>:] with Glassed[=>:] {
+trait Windowed[=>:[-_,+_]] extends CoCartesianChoice[=>:] with Glassed[=>:] {
   def windowedRGL[A,B,U,V,T]: (A =>: B) => (Either[V, (T,U=>A)] =>: Either[V, (T,U=>B)]) =
     right compose glassedL
   def windowedRGR[A,B,U,V,T]: (A =>: B) => (Either[V, (U=>A,T)] =>: Either[V, (U=>B,T)]) =

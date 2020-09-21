abb5562a6cc397ae3e7bad603d257c17e2f68e9a
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-09-21 17:12:14 +0200

    define laws for type classes with 3 holes

--- src/main/scala/Triglav/face3/Bard.scala
+++ src/main/scala/Triglav/face3/Bard.scala
@@ -4,3 +4,31 @@ package Triglav.face3
 trait Bard[F[-_,_,_]] {
   def contramap[E,A,R,EE](fa: F[E,A,R])(f: EE => E): F[EE,A,R]
 }
+
+trait BardLaws[P[-_,_,_]]
+  extends Bard[P] {
+
+  // contramap id == id
+  def contramapIdentity[A,B,C](p: P[A,B,C]): Boolean = {
+    //          contramap(id)
+    // P[A,B] ================> P[A,B]
+    contramap(p)(identity[A]) == p
+  }
+
+  // contramap (f . g) == contramap g . contramap f
+  def contramapComposition[A,A2,A3,B,B2,B3,C](p: P[A,B,C], g: A3 => A2, f: A2 => A): Boolean = {
+    //          contramap B=>B2
+    // P[A,B,C] ===================> F[A2,B2,C]
+    val p2: P[A2,B,C] = contramap(p)(f)
+    //          contramap B2=>B3
+    // P[A2,B2,C] ====================> P[A3,B3,C]
+    val l: P[A3,B,C] = contramap(p2)(g)
+
+    val fg: A3 => A = f compose g
+    //         contramap B=>B3
+    // P[A,B,C] ===================> P[A3,B3,C]
+    val r: P[A3,B,C] = contramap(p)(fg)
+
+    l == r
+  }
+}
--- src/main/scala/Triglav/face3/Trifunctor.scala
+++ src/main/scala/Triglav/face3/Trifunctor.scala
@@ -10,6 +10,7 @@ trait Trifunctor[F[-_,+_,+_]]
     g: A => AA,
     h: R => RR): F[EE,AA,RR]
 
+  // derived methods
   override def contramap[E, A, R, EE](fa: F[E, A, R])(f: EE => E): F[EE, A, R] =
     timap(fa)(f, identity[A], identity[R])
 
@@ -27,4 +28,50 @@ trait Trifunctor[F[-_,+_,+_]]
 
   override def dimap[E, A, R, EE, RR](fa: F[E, A, R])(f: EE => E, h: R => RR): F[EE, A, RR] =
     timap(fa)(f, identity[A], h)
+}
+
+trait ProfunctorLaws[P[-_,+_,+_]]
+  extends JokerLaws[P]
+    with BardLaws[P]
+    with ClownLaws[P]
+    with Trifunctor[P] {
+
+  // dimap id id == id
+  def timapIdentity[A, B, C](p: P[A, B, C]): Boolean = {
+    //          dimap(id, id, id)
+    // P[A,B,C] ================> P[A,B,C]
+    timap(p)(identity[A], identity[B], identity[C]) == p
+  }
+
+  // timap (f . g) (h . i) == dimap g h . dimap f i
+  def timapComposition[A, B, C, A2, A3, B2, B3, C2, C3](pad: P[A, B, C], g: A3 => A2, f: A2 => A, i: B => B2, h: B2 => B3, k: C => C2, m: C2 => C3): Boolean = {
+    //          timap A2=>A B=>B2 C=>C2
+    // P[A,B] ==========================> F[A2,B2]
+    val p2: P[A2, B2, C2] = timap(pad)(f, i, k)
+    //          timap A3=>A2 B2=>B3
+    // P[A2,B2] ====================> P[A3,B3]
+    val l: P[A3, B3, C3] = timap(p2)(g, h, m)
+
+    val fg: A3 => A = f compose g
+    val hi: B => B3 = h compose i
+    val mk: C => C3 = m compose k
+    //         timap A3=>A B=>B3
+    // P[A,B] ===================> P[A3,B3]
+    val r: P[A3, B3, C3] = timap(pad)(fg, hi, mk)
+
+    l == r
+  }
+
+  // dimap f g == contramap (map g) f
+  def timapCoherentWithMapAndContramap[A, B, C, A2, B2, C2](pad: P[A, B, C], f: A2 => A, g: B => B2, h: C => C2): Boolean = {
+    //          dimap A2=>A B=>B2
+    // P[A,B] ===================> F[A2,B2]
+    val l: P[A2, B2, C2] = timap(pad)(f, g, h)
+
+    val r1 = map(pad)(h)
+    val r2 = contramap(r1)(f)
+    val r3 = mapLeft(r2)(g)
+
+    l == r3
+  }
 }

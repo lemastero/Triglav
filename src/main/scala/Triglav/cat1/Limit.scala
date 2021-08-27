7a7aa54d91096e67d2c65573c0796c7d37f4e470
Scala Steward <me@scala-steward.org>
2021-08-27 21:40:27 +0200

    Reformat with scalafmt 3.0.1

--- src/main/scala/Triglav/cat1/Limit.scala
+++ src/main/scala/Triglav/cat1/Limit.scala
@@ -17,21 +17,21 @@ object Limit {
 
   /** Terminal object (final object)
     *
-    * Given C be a Category. Terminal object in C is object T
-    * such that (universal property) given any other object X
-    * there is unique morphism from X to terminal object.
+    * Given C be a Category. Terminal object in C is object T such that
+    * (universal property) given any other object X there is unique morphism
+    * from X to terminal object.
     *
-    *  X -----> T
+    * X -----> T
     *
     * Examples:
-    * - Set: every set with 1 element e.g. {42}
-    * - Grp: trivial group
-    * - Top: one point space
-    * - Poset: maximum element (if exists)
+    *   - Set: every set with 1 element e.g. {42}
+    *   - Grp: trivial group
+    *   - Top: one point space
+    *   - Poset: maximum element (if exists)
     *
     * Non examples:
-    * - Natural numbers (no max)
-    * - Vector spaces
+    *   - Natural numbers (no max)
+    *   - Vector spaces
     *
     * Terminal objects in C are unique up to unique isomorphism.
     *
@@ -44,58 +44,44 @@ object Limit {
 
   /** Given: object C and morphisms: A => C and B => C
     *
-    *           B
-    *           |
-    *           |   g
-    *          \/
-    * A -----> C
-    *     f
+    * B
+    * |
+    * | g \/ A -----> C f
     *
     * a pull back (or fiber product) is:
-    * - object U
-    * - morphisms U => B and U => A
+    *   - object U
+    *   - morphisms U => B and U => A
     *
-    *       p
-    *  U ------> B
-    *  |         |
-    *  |  q      |   g
-    *  \/        \/
-    *  A ------> C
-    *      f
+    * p U ------> B
+    * |   |
+    * |:--|
+    * | q |
+    * g \/ \/ A ------> C f
     *
-    * such that (universal property of pullback)
-    * for all other V, s: V => B, t: V => A
+    * such that (universal property of pullback) for all other V, s: V => B, t:
+    * V => A
     *
-    *        t
-    *   V -------------+
-    *   |              |
-    *   |        p    \/
-    *   |    U ------> B
-    * s |    |         |
-    *   |  q |         |   g
-    *   |    \/  f     \/
-    *   +---> A ------> C
+    * t V -------------+
+    * | |
+    * | p \/
+    * | U ------> B s | | |
+    * | q | | g
+    * | \/ f \/
+    * +---> A ------> C
     *
-    * exists unique morphism h: V => U such that
-    * following diagrams commute:
+    * exists unique morphism h: V => U such that following diagrams commute:
     *
-    *        t
-    *   V -------------+
-    *   | \            |
-    *   |  \/     p    \/
-    *   |    U ------> B
-    * s |    |         |
-    *   |  q |         |   g
-    *   |    \/  f     \/
-    *   +---> A ------> C
-    *            f
+    * t V -------------+
+    * | \ |
+    * | \/ p \/
+    * | U ------> B s | | |
+    * | q | | g
+    * | \/ f \/
+    * +---> A ------> C f
     *
-    * Pullback is limit where diagram has 3 objects A, B, C
-    * and morphisms A => C and B => C
-    *        B
-    *        |
-    *        \/
-    * A ---> C
+    * Pullback is limit where diagram has 3 objects A, B, C and morphisms A => C
+    * and B => C B
+    * | \/ A ---> C
     */
   abstract class Pullback[A, B, +C](val f: A => C, val g: B => C)
       extends Limit {
@@ -107,11 +93,10 @@ object Limit {
       (q andThen f)(u) == (p andThen g)(u)
   }
 
-  /** Equalizer is a limit where diagram has 2 objects A, B
-    * and 2 morphisms A => B
+  /** Equalizer is a limit where diagram has 2 objects A, B and 2 morphisms A =>
+    * B
     *
-    *   --->
-    * A ---> B
+    * ---> A ---> B
     */
   abstract class Equalizer[X, +Y](val f: X => Y, val g: X => Y) extends Limit {
     type E
